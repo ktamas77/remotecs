8ba2860211bc016378df49ea6db2d28b18cf673c
Tamas Kalman <tamas@mobileplusinc.com>
2013-02-08 14:20:11 -0800

    initial commit

---
+++ Payload.class.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+ * GitHUB PayLoad Class Handler
+ *
+ * @author Tamas Kalman <ktamas77@gmail.com>
+ */
+Class Payload {
+
+    var $payloadPost;
+    var $payload;
+    var $logDir;
+
+    function __construct() {
+        $this->payloadPost = isset($_POST['payload']) ? $_POST['payload'] : false;
+        $this->payload = ($this->payloadPost) ? json_decode($payloadPost, true) : false;
+        $this->setLogDir(__DIR__ . DIRECTORY_SEPARATOR . 'log');
+    }
+
+    public function getPayLoad() {
+        return $this->payload;
+    }
+
+    public function setLogDir($logDir) {
+        $this->logDir = $logDir;
+    }
+
+    /**
+     * Logs the raw request
+     *
+     * @param String $logFile Log Filename (optional)
+     *
+     * @return void
+     */
+    public function log($logFile = null) {
+        if (!is_dir($this->logDir)) {
+            mkdir($this->logDir);
+        }
+        $logFile = $logFile ?: sprintf('post-%s-%s.log', time(), rand(100000, 999999));
+        file_put_contents($this->logDir . DIRECTORY_SEPARATOR . $logFile, $this->payloadPost);
+    }
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * RemoteCS - Convenient Remote Coding Standards Validation
+ *
+ * @author Tamas Kalman <ktamas77@gmail.com>
+ */
+
+require_once 'Payload.class.php';
+
+$payload = new Payload();
+$payload->log();
+
